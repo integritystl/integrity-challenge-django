df40697ac459d1f90281e21e93b12150a5aa6408
Matthew Morgan <m_kmorgan@hotmail.com>
2024-11-01 16:01:12 -0600

    FEAT: created url for add_comment

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -5,9 +5,13 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
+    # Blog Posts
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('test-auth/', views.test_auth, name='test_auth'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<slug:slug>/edit/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<slug:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+
+    # Blog Post Comments
+    path('post/<int:post_id>/comment/', views.add_comment, name='add_comment'),
 ]
